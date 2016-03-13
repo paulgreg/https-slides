42db25acd17d6520e798edcc28cbdc6788b286f2
Grégory Paul <paulgreg@gmail.com>
2016-03-13 19:00:34 +0100

    Use a webcomponent as slide progression

---
+++ slide-progression.js
@@ -0,0 +1,21 @@
+(function () {
+    var proto = Object.create(HTMLElement.prototype);
+
+    proto.createdCallback = function() {
+        var tpl = document.querySelector('#slide-progression-tpl');
+        var clone = document.importNode(tpl.content, true);
+
+        var selection = this.getAttribute('open');
+
+        var liToShow = clone.querySelector('.'+selection);
+        liToShow.classList.remove('closed');
+        liToShow.classList.remove('hidden');
+
+        this.appendChild(clone);
+    };
+
+    var SlidesProgression = document.registerElement('slide-progression', {
+        prototype: proto
+    });
+
+})();
